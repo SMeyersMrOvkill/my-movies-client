e0b8bdcae07a7d8e63b26341b7f900f1543eb47c
Samuel L Meyers <thecheesecakejunkie@gmail.com>
2021-02-10 13:35:50 -0600

    Set server to deployed heroku server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 let config = {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  //API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://capstone-3-my-movies.herokuapp.com/api',
   TOKEN_KEY: 'my-movies-client-auth-token',
 };
 
